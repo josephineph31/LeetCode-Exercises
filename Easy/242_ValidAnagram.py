0ff3e59bbdde54bd5c07ce1f41b905441e7957ef
Ngoc Pham <baongocyu@gmail.com>
2025-06-30 03:50:48 +0900

    Add files via upload

---
+++ Easy/242_ValidAnagram.py
@@ -0,0 +1,11 @@
+def isAnagram(s, t):
+    if len(s) != len(t):
+        return False
+    countS, countT = {}, {}
+    for i in range(len(s)):
+        countS[s[i]] = 1 + countS.get(s[i], 0)
+        countT[t[i]] = 1 + countT.get(t[i], 0)
+    return countS == countT
+s = 'codingisfun'
+t = 'iscodingfun'
+print(isAnagram(s,t))
